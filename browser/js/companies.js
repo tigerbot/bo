8b1c77642129c696be9076c1de40850783e6f8f3
Tigerbot <slgibelyou@gmail.com>
2016-07-13 21:23:38 -0600

    formatted the company list

--- browser/js/companies.js
+++ browser/js/companies.js
@@ -5,16 +5,16 @@
 	var request  = require('./json-request');
 
 	var company_colors = {
-		'pennsylvania':                 'red',
-		'boston_maine':                 'pink',
-		'illinois_central':             'orange',
-		'chesapeake_ohio':              'yellow',
-		'new_york_central':             'green',
-		'baltimore_ohio':               'blue',
-		'new_york_chicago_saint_louis': 'purple',
-		'erie':                         'brown',
-		'wabash':                       'grey',
-		'new_york_new_haven_hartford':  'white',
+		'pennsylvania':                 'rgb(255,  48,  48)',
+		'boston_maine':                 'rgb(255, 128, 192)',
+		'illinois_central':             'rgb(255, 128,   0)',
+		'chesapeake_ohio':              'rgb(248, 248,   7)',
+		'new_york_central':             'rgb( 64, 224,  96)',
+		'baltimore_ohio':               'rgb(  0, 128, 255)',
+		'new_york_chicago_saint_louis': 'rgb(128,  64, 255)',
+		'erie':                         'rgb(160,  80,  0)',
+		'wabash':                       'rgb(100, 100, 100)',
+		'new_york_new_haven_hartford':  'rgb(255, 255, 255)',
 	};
 	var company_list = ko.observableArray();
 
@@ -31,10 +31,6 @@
 			view_model.name = name;
 			view_model.icon = '/logos/'+safe_name+'.png';
 			view_model.color = company_colors[safe_name];
-
-			view_model._president = ko.computed(function () {
-				return view_model.president.name() + ' (' + view_model.president.shares() + ')';
-			}, view_model);
 
 			company_list.push(view_model);
 		});
