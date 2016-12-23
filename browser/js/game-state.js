85e36afd426a4a2d8fd6e00ebe6e1a956d335ce5
Tigerbot <slgibelyou@gmail.com>
2016-12-23 10:29:18 -0700

    moved handling of game state in UI to another file

---
+++ browser/js/game-state.js
@@ -0,0 +1,102 @@
+(function () {
+	'use strict';
+	var ko       = require('knockout');
+	var domready = require('domready');
+
+	var common    = require('./common');
+	var hex_map   = require('./hex-map');
+
+	var game_state = {
+		round: ko.observable(0),
+		phase: ko.observable(""),
+		turn:  ko.observable(""),
+
+		tech_level:    ko.observable(1),
+		trains_bought: ko.observable(0),
+		all_costs:     ko.observableArray([]),
+
+		orphan_stocks: ko.observableArray([]),
+	};
+	game_state.train_costs = ko.computed(function () {
+		var tech_level = this.tech_level();
+		var all_costs  = this.all_costs();
+		var result = [];
+
+		if (tech_level <= all_costs.length) {
+			result.push(all_costs[tech_level-1]);
+		}
+		if (tech_level < all_costs.length) {
+			result.push(all_costs[tech_level]);
+		}
+
+		return result;
+	}, game_state);
+
+	domready(function () {
+		ko.applyBindings(game_state, document.getElementById('game-state'));
+	});
+
+	common.request('/train_costs', function (err, repsonse) {
+		if (err) {
+			console.error('failed to get equipment costs', err);
+			return;
+		}
+		game_state.all_costs(repsonse.map(function (costs, lvl) {
+			return {
+				name:  'Tech ' + (lvl+1),
+				costs: costs.map(function (value, num) {
+					return {
+						value:         value,
+						number:        lvl*costs.length + num,
+						trains_bought: game_state.trains_bought,
+					};
+				}),
+			};
+		}));
+	});
+
+	function log_error(err) {
+		if (err) {
+			console.error('failed to get game state', err);
+		}
+	}
+
+	function refresh(cb) {
+		if (typeof cb !== 'function') {
+			cb = log_error;
+		}
+
+		common.request('/game/state', function (err, repsonse) {
+			if (err) {
+				return cb(err);
+			}
+
+			game_state.round(repsonse.round);
+			game_state.phase(repsonse.phase);
+			game_state.turn(repsonse.turn);
+
+			game_state.tech_level(repsonse.tech_level);
+			game_state.trains_bought(repsonse.trains_bought);
+
+			game_state.orphan_stocks(Object.keys(repsonse.orphan_stocks).map(function (name) {
+				return {
+					name:  name,
+					color: common.get_company_color(name),
+					count: repsonse.orphan_stocks[name],
+				};
+			}));
+
+			hex_map.set_coal(repsonse.unmined_coal);
+
+			cb();
+		});
+	}
+
+	module.exports.refresh = refresh;
+	Object.defineProperty(module.exports, 'phase', {
+		get: function () { return game_state.phase(); },
+	});
+	Object.defineProperty(module.exports, 'turn', {
+		get: function () { return game_state.turn(); },
+	});
+})();
--- browser/js/index.js
+++ browser/js/index.js
@@ -1,82 +1,30 @@
 (function () {
 	'use strict';
 	require('knockout-mapping');
-	var ko       = require('knockout');
-	var domready = require('domready');
 
-	var common    = require('./common');
+	var state     = require('./game-state');
 	var hex_map   = require('./hex-map');
 	var players   = require('./players');
 	var companies = require('./companies');
 
-	var game_state = {
-		round:      ko.observable(0),
-		phase:      ko.observable(""),
+	var market_regexp   = /^market/i;
+	var business_regexp = /^business/i;
 
-		tech_level:    ko.observable(1),
-		trains_bought: ko.observable(0),
-		all_costs:     ko.observableArray([]),
-
-		orphan_stocks: ko.observableArray([]),
-	};
-	game_state.train_costs = ko.computed(function () {
-		var tech_level = this.tech_level();
-		var all_costs  = this.all_costs();
-		var result = [];
-
-		if (tech_level <= all_costs.length) {
-			result.push(all_costs[tech_level-1]);
-		}
-		if (tech_level < all_costs.length) {
-			result.push(all_costs[tech_level]);
-		}
-
-		return result;
-	}, game_state);
-
-	domready(function () {
-		ko.applyBindings(game_state, document.getElementById('game-state'));
-	});
-	common.request('/train_costs', function (err, repsonse) {
+	function check_action(err) {
 		if (err) {
-			console.error('failed to get equipment costs', err);
-			return;
-		}
-		game_state.all_costs(repsonse.map(function (costs, lvl) {
-			return {
-				name:  'Tech ' + (lvl+1),
-				costs: costs.map(function (value, num) {
-					return {
-						value:         '$'+value,
-						number:        lvl*costs.length + num,
-						trains_bought: game_state.trains_bought,
-					};
-				}),
-			};
-		}));
-	});
-	common.request('/game/state', function (err, repsonse) {
-		if (err) {
-			console.error('failed to get game global state', err);
-			return;
+			console.error('failed to determine what turn needs to be taken', err);
 		}
 
-		game_state.round(repsonse.round);
-		game_state.phase(repsonse.phase);
-		game_state.tech_level(repsonse.tech_level);
-		game_state.trains_bought(repsonse.trains_bought);
-		Object.keys(repsonse.orphan_stocks).forEach(function (name) {
-			game_state.orphan_stocks.push({
-				name:  name,
-				color: common.get_company_color(name),
-				count: repsonse.orphan_stocks[name],
-			});
-		});
+		if (market_regexp.test(state.phase)) {
+			console.log("Market phase on "+state.turn+"'s turn");
+		} else if (business_regexp.test(state.phase)) {
+			console.log("Business phase for company "+state.turn);
+		}
+	}
 
-		hex_map.set_coal(repsonse.unmined_coal);
-	});
+	function next_turn() {
+		state.refresh(check_action);
+	}
 
-	window.hex_map   = hex_map;
-	window.players   = players;
-	window.companies = companies;
+	next_turn();
 })();
